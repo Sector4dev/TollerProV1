659880e1783a63090f58ba84a436a9a51ea31809
Shank93 <najaf.k.a@gmail.com>
2017-07-26 16:30:31 +0530

    v1

--- app/src/main/java/com/tollerpro/sector4dev/tollerprov1/TollerproMainActivity.java
+++ app/src/main/java/com/tollerpro/sector4dev/tollerprov1/TollerproMainActivity.java
@@ -2,12 +2,30 @@ package com.tollerpro.sector4dev.tollerprov1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
-public class TollerproMainActivity extends AppCompatActivity {
-
+public class TollerproMainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+    ToggleButton togglemutebttn,toggleamplibttn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tollerpro_main);
+
+        togglemutebttn=(ToggleButton)findViewById(R.id.buttonmute);
+        togglemutebttn.setOnCheckedChangeListener(this);
+
+        toggleamplibttn=(ToggleButton)findViewById(R.id.buttonamplifier);
+        toggleamplibttn.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compund, boolean isChecked) {
+        if (isChecked){
+            Toast.makeText(this,"enabled",Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this,"disabled",Toast.LENGTH_SHORT).show();
+        }
     }
 }
